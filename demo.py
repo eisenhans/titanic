3b2f330ef435881f6febc1b402be2de3953ffc82
Markus Loeffler <maloef@gmail.com>
2018-02-20 19:14:30 +0000

    implement k-fold cross validation, use all data for training and validation

--- demo.py
+++ demo.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import display
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.callbacks import ModelCheckpoint
+
 
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_columns', 0)
@@ -86,56 +88,91 @@ print(main_df.head(20))
 
 # split data in training/validation set (random)
 # TODO: better use cross-validation
-temp_df, test_local_df = train_test_split(main_df, test_size=0.2)
-train_df, val_df = train_test_split(temp_df, test_size=0.25)
-print('train/validate/test/total data: {}/{}/{}/{}'.format(len(train_df), len(val_df), len(test_df), len(main_df)))
+# temp_df, test_local_df = train_test_split(main_df, test_size=0.2)
+# train_df, val_df = train_test_split(temp_df, test_size=0.25)
+# print('train/validate/test/total data: {}/{}/{}/{}'.format(len(train_df), len(val_df), len(test_df), len(main_df)))
 
 target_col = 'Survived'
-x_train = train_df.drop(target_col, axis=1).values
-y_train = train_df[target_col].values
+x = main_df.drop(target_col, axis=1).values
+y = main_df[target_col].values
+# x_train = train_df.drop(target_col, axis=1).values
+# y_train = train_df[target_col].values
 
-x_val = val_df.drop(target_col, axis=1).values
-y_val = val_df[target_col].values
-x_local_test = test_local_df.drop(target_col, axis=1).values
-y_local_test = test_local_df[target_col].values
+# x_val = val_df.drop(target_col, axis=1).values
+# y_val = val_df[target_col].values
+# x_local_test = test_local_df.drop(target_col, axis=1).values
+# y_local_test = test_local_df[target_col].values
 
 # define neural network
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=x_train.shape[1]))
-model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
+def create_model(input_dim):
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_dim=input_dim))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    # model.add(Dense(128, activation='relu'))
+    # model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    # model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-weights_file = 'output/weights_best_model.hdf5'
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-checkpointer = ModelCheckpoint(filepath=weights_file, verbose=1, save_best_only=True)
+    return model
 
-# train model
-hist = model.fit(x_train, y_train, batch_size=32, epochs=20, validation_data=(x_val, y_val), callbacks=[checkpointer],
-                 verbose=2)
 
-model.load_weights(weights_file)
+def train_model(model, x_train, y_train, validation_split=None):
+    if validation_split:
+        weights_file = 'output/weights_best_model.hdf5'
+        callbacks = [ModelCheckpoint(filepath=weights_file, verbose=1, save_best_only=True)]
+    else:
+        callbacks = None
 
-train_score = model.evaluate(x_train, y_train, verbose=0)
-val_score = model.evaluate(x_val, y_val, verbose=0)
-test_score = model.evaluate(x_local_test, y_local_test, verbose=0)
-print("Training/validation/test accuracy: ", train_score[1], val_score[1], test_score[1])
+    hist = model.fit(x_train, y_train, batch_size=32, epochs=40, callbacks=callbacks,
+                     validation_split=validation_split, verbose=0)
 
-# TODO: create output for upload
+    # if validation_split:
+    #     model.load_weights(weights_file)
+
+
+def evaluate_model(model, x_val, y_val):
+    score = model.evaluate(x_val, y_val, verbose=0)
+    return score
+
+
+def predict(model, x_test):
+    prediction = model.predict(x_test)
+    return (prediction >= 0.5).astype(int)
+
+
+# seed = 7
+kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=None)
+scores = []
+
+for train, val in kfold.split(x, y):
+    model = create_model(x.shape[1])
+    train_model(model, x[train], y[train])
+    score = evaluate_model(model, x[val], y[val])
+    print('score: {}'.format(score[1]))
+    scores.append(score[1])
+
+
+print('score avg: {}, score stdv: {}'.format(np.mean(scores), np.std(scores)))
+
+final_model = create_model(x.shape[1])
+train_model(final_model, x, y, validation_split=0.33)
+final_model.load_weights('output/weights_best_model.hdf5')
+
 test_passenger_ids = test_df.PassengerId.values
 test_passenger_ids.shape = (test_passenger_ids.shape[0], 1)
 test_df = prepare_data(test_df)
-if not 'Cabin_T' in test_df:
+if 'Cabin_T' not in test_df:
     test_df['Cabin_T'] = 0
 x_test = test_df.values
-prediction = model.predict(x_test)
-pred_int = (prediction >= 0.5).astype(int)
 
-
-result = np.concatenate((test_passenger_ids, pred_int), axis=1)
+prediction = predict(final_model, x_test)
+result = np.concatenate((test_passenger_ids, prediction), axis=1)
 np.savetxt("output/result.csv", result, delimiter=",", fmt='%1i', header='PassengerId,Survived', comments='')
+
+
 
 # TODO: visualize: what whas right/wrong?
